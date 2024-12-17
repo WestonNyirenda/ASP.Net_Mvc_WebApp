77350848f1a80a9cc04eca722de478a76e07a3ce
RP <RP@DESKTOP-PGELF4O>
2024-12-17 14:43:31 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+         public IActionResult Index()
+         {  
+             return View();
+         }
+
+
+        //GET: HelloWorld/ 
+        /*public string index()
+        {
+            return "This is my default action";
+        }*/
+
+        //GET: HelloWorld/welcome
+         public IActionResult Welcome(string name, int numTimes = 3)
+         {
+            ViewData["message"] = "Hello " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+        }
+    }
+}
